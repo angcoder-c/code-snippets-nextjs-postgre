14483fb6f0f1dc85cb5a65401f6cf1a37999d283
angc <angcoder.c@gmail.com>
2025-06-16 20:09:25 -0600

    solve bug with redirect

--- src/components/auth/authButton.tsx
+++ src/components/auth/authButton.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { useSession, signIn, signOut } from "next-auth/react"
-import { redirect } from "next/navigation"
 
 export default function AuthButton () {
     const {data: session, status} = useSession()

d2292f82bea987bbff63e5910a98cf9b5b8bb99f
angc <angcoder.c@gmail.com>
2025-06-19 23:25:38 -0600

    solve prisma export bug

--- src/app/lib/actions.ts
+++ src/app/lib/actions.ts
@@ -1,8 +1,10 @@
 'use server'
 
 import prisma from "./prisma";
+import { SnippetType } from "@/types";
 
 export async function fetchSnippets() {
     const snippets = await prisma.snippet.findMany()
+    //const snippets = await prisma.
     return snippets
 }
